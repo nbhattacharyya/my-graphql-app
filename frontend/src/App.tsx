3f0f7af3d803c81ccc3c895f95eb0b17252233ab
nbhattacharyya <neel@boundlessrider.com>
2025-05-30 11:56:21 -0500

    frontend dependencies

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,9 +1,7 @@
 
 import './App.css'
-import { useState, useEffect, use } from 'react'
+import { useState, useEffect } from 'react'
 import { GraphQLClient } from "graphql-request"
-import { Tabs, TabsList, TabsTrigger, TabsContent } from './components/ui/tabs'
-import { Button } from './components/ui/button'
 
 const API_URL = import.meta.env.VITE_APP_GRAPHQL_URL;
 const API_KEY = import.meta.env.VITE_APP_API_KEY;
@@ -37,36 +35,30 @@ interface Game {
 
 function App() {
   const [games, setGames] = useState<Game[]>([]);
-  const [selectedSport, setSelectedSport] = useState('');
-  const [loading, setLoading] = useState(true);
-  const [error, setError] = useState(null);
 
   useEffect(() => {
     const fetchGames = async () => {
       try {
-        setLoading(true);
-        setError(null);
         const data = await graphqlClient.request<{ listGamesToday: Game[] }>(LIST_GAMES_QUERY);
         setGames(data.listGamesToday);
       } catch (err: any) {
         console.error('Error fetching games: ', err);
-        setError(err.message || 'failed to fetch games');
-      } finally {
-        setLoading(false);
       }
     }
     fetchGames();
   }, []);
 
-  const sports = [...new Set(games?.map(game => game.sport))];
-  const filteredGames = games?.filter((game: Game) => game.sport === selectedSport);
-
   return (
-    <>
-      <div className='container mx-auto p-4 max-w-4xl'>
-        <h1 className='text-4xl font-bold text-center mb-8'>Today's Ganes</h1>
+    <div className='container mx-auto p-4 max-w-4xl'>
+      <h1 className='text-4xl font-bold text-center mb-8'>Today's Games</h1>
+      <div>
+        {games.map(game => (
+          <div key={game.id} className="border p-4 mb-2">
+            {game.homeTeam} vs {game.awayTeam} - {game.sport}
+          </div>
+        ))}
       </div>
-    </>
+    </div>
   )
 }
 
